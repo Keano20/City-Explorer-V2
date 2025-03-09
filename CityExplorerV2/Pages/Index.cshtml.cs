138900476e934f2136aa5ec58732057533ed545f
Keano Ricketts <keano.ricketts@yahoo.com>
2025-03-09 15:26:09 +0000

    Add static files for City Explorer interface (HTML, CSS, JS)

---
+++ CityExplorerV2/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityExplorerV2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            // This method runs when the page is accessed via a GET request
+        }
+    }
+}
